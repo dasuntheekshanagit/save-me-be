4f001c9143863e8334238505f22ec065da63f3e8
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2025-11-28 14:22:48 +0530

    [dasun] fix lombock issue

--- src/main/java/com/saveme/saveme/dto/SummaryDto.java
+++ src/main/java/com/saveme/saveme/dto/SummaryDto.java
@@ -1,10 +1,31 @@
 package com.saveme.saveme.dto;
 
-import lombok.Data;
-
-@Data
 public class SummaryDto {
     private long tapped;
     private long missing;
     private long criticalAlerts;
+
+    public long getTapped() {
+        return tapped;
+    }
+
+    public void setTapped(long tapped) {
+        this.tapped = tapped;
+    }
+
+    public long getMissing() {
+        return missing;
+    }
+
+    public void setMissing(long missing) {
+        this.missing = missing;
+    }
+
+    public long getCriticalAlerts() {
+        return criticalAlerts;
+    }
+
+    public void setCriticalAlerts(long criticalAlerts) {
+        this.criticalAlerts = criticalAlerts;
+    }
 }
--- src/main/java/com/saveme/saveme/model/User.java
+++ src/main/java/com/saveme/saveme/model/User.java
@@ -3,14 +3,36 @@ package com.saveme.saveme.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 @Entity
-@Data
 @Table(name = "users")
 public class User {
     @Id
     private String email;
     private String name;
     private String phoneNumber;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
