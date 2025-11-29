1f8c1983eb6ee774645ad1ff612ef8d861797823
Dasun Theekshana <dasun.theekshana.git@gmail.com>
2025-11-29 10:07:58 +0530

    [dasun] update security config

--- src/main/java/com/saveme/saveme/config/SecurityConfig.java
+++ src/main/java/com/saveme/saveme/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Allow your React app's origin
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://savesrilanka.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
